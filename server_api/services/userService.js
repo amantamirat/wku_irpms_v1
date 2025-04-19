8b2bf758a3f393a94fd0db81947be6450c3be414
amantamirat <amweezy2@gmail.com>
2025-04-19 21:58:02 +0300

    node mailer added

--- server_api/services/userService.js
+++ server_api/services/userService.js
@@ -5,4 +5,35 @@ const prepareHash = async (password) => {
     return await bcrypt.hash(password, salt);
 };
 
+const sendEmail = async (email, code) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            service: "Gmail",
+            auth: {
+                user: process.env.USER,
+                pass: process.env.PASS,
+            }
+        });
+        const myOptions = {
+            from: process.env.USER,
+            to: email,
+            subject: "Email Verification",
+            text: "Hello Welcome, Here is Verfication Code to Activate Your Account ",
+            html: " <h2>" + code + "</h2>"
+        }
+        transporter.sendMail(myOptions, function (error, info) {
+            if (error) {
+                //if email is not found delete the user
+                console.log(error);
+                //throw error;
+            } else {
+                console.log("Email Sent:" + info.response);
+            }
+        });
+
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+};
 module.exports = {prepareHash};
