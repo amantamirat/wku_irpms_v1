1dbff2578981294e17a8b4b288995716ac015563
unknown <amweezy2@gmail.com>
2025-05-19 09:10:28 +0300

    sub area model added

--- server_api/src/models/theme/subArea.model.ts
+++ server_api/src/models/theme/subArea.model.ts
@@ -1,14 +1,14 @@
 import mongoose, { Schema, model, Document } from 'mongoose';
 
 export interface ISubArea extends Document {
-    priority_area: mongoose.Types.ObjectId;
+    priorityArea: mongoose.Types.ObjectId;
     title: string;
     createdAt?: Date;
     updatedAt?: Date;
 }
 
 const SubAreaSchema = new Schema<ISubArea>({
-    priority_area: {
+    priorityArea: {
         type: Schema.Types.ObjectId,
         ref: 'PriorityArea',
         required: true
