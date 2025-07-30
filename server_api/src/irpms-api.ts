0595eaaf452e62ede001e7160b66fc89ccd773a3
unknown <amweezy2@gmail.com>
2025-07-30 15:31:43 +0300

    old models and pages removed from back end

--- server_api/src/irpms-api.ts
+++ server_api/src/irpms-api.ts
@@ -10,36 +10,12 @@ import applicantRoutes from './modules/applicants/applicant.routes';
 import themeRoutes from './modules/theme/theme.routes';
 import evalRoutes from './modules/evaluation/evaluation.routes';
 
-
-
-import collegeRoutes from './routes/collegeRoutes';
-import departmentRoutes from './routes/departmentRoutes';
-import programRoutes from './routes/programRoutes';
-import specializationRoutes from './routes/specializationRoutes';
-import directorateRoutes from './routes/directorateRoutes';
 import calendarRoutes from './routes/calendarRoutes';
-import positionRoutes from './routes/positionRoutes';
-import rankRoutes from './routes/rankRoutes';
-
 import callRoutes from './routes/call.routes';
-
-import thmRoutes from './routes/theme/theme.routes';
-import priorityAreaRoutes from './routes/theme/priorityArea.routes';
-import subAreaRoutes from './routes/theme/subArea.routes';
-
-import evaluationRoutes from './routes/evaluation/evaluation.routes';
-import stageRoutes from './routes/evaluation/stage.routes';
-import weightRoutes from './routes/evaluation/weight.routes';
-import criterionOptionRoutes from './routes/evaluation/criterionOption.routes';
-
-import { seedPositions, seedSectors } from './services/seedService';
 import { initAdminUser, initPermissions, initRoles } from './services/initService';
 
 
-
-
 dotenv.config();
-
 const app: Application = express();
 
 app.use(cors());
@@ -54,25 +30,15 @@ app.use("/api/eval", evalRoutes);
 
 
 
-app.use("/api/colleges", collegeRoutes);
-app.use("/api/departments", departmentRoutes);
-app.use("/api/specializations", specializationRoutes);
-app.use("/api/programs", programRoutes);
-app.use("/api/directorates", directorateRoutes);
+
 app.use("/api/calendars", calendarRoutes);
-app.use("/api/positions", positionRoutes);
-app.use("/api/ranks", rankRoutes);
+
 app.use("/api/applicants", applicantRoutes);
 app.use("/api/calls", callRoutes);
 
-app.use("/api/thms", thmRoutes);
-app.use("/api/priorityAreas", priorityAreaRoutes);
-app.use("/api/subAreas", subAreaRoutes);
 
-app.use("/api/evaluations", evaluationRoutes);
-app.use("/api/stages", stageRoutes);
-app.use("/api/weights", weightRoutes);
-app.use("/api/criterionOptions", criterionOptionRoutes);
+
+
 
 const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.SERVER_PORT || 5000;
@@ -87,9 +53,6 @@ const PORT = process.env.SERVER_PORT || 5000;
     await initPermissions();
     await initRoles();
     await initAdminUser();
-    
-    await seedPositions();
-    await seedSectors();
     app.listen(PORT, () => {
       console.log(`Server API is running at http://127.0.0.1:${PORT}`);
     });
