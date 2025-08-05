7756d56bcef5d754ff81e9e48241340cb619c141
unknown <amweezy2@gmail.com>
2025-08-05 10:54:00 +0300

    sucesss field added to sucess and error responses

--- server_api/src/util/response.ts
+++ server_api/src/util/response.ts
@@ -1,9 +1,9 @@
 import { Response } from 'express';
 
 export const successResponse = (res: Response, statusCode: number = 200, message: string, data?: any): void => {
-    res.status(statusCode).json({ message, data });
+    res.status(statusCode).json({ success:true, message, data });
 };
 
 export const errorResponse = (res: Response, statusCode: number = 401, message: string, error?: any): void => {
-    res.status(statusCode).json({ message, error });
+    res.status(statusCode).json({ success:false, message, error });
 };
