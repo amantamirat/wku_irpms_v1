ef6ae71861a36595fe04b2d8fe103d697a12f18d
amantamirat <amweezy2@gmail.com>
2025-08-23 14:27:55 +0300

    project added

---
+++ server_api/src/modules/project/project.model.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+import { COLLECTIONS } from "../../enums/collections.enum";
+
+interface IProject extends Document {
+    call: mongoose.Types.ObjectId;
+    title: string;
+    summary?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const ProjectSchema = new Schema<IProject>({
+    call: {
+        type: Schema.Types.ObjectId,
+        ref: COLLECTIONS.CALL,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+    }
+}, { timestamps: true });
