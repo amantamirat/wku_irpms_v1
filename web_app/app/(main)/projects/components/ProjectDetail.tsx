74e5e2eb94ab0be3d60cf534866d7b651fc5d985
unknown <amweezy2@gmail.com>
2025-09-17 09:43:01 +0300

    project detail added

--- web_app/app/(main)/projects/components/ProjectDetail.tsx
+++ web_app/app/(main)/projects/components/ProjectDetail.tsx
@@ -9,29 +9,33 @@ interface ProjectDetailProps {
 
 export default function ProjectDetail({ project }: ProjectDetailProps) {
     return (
-        <div className="p-4">
+        <div className="project-detail">
 
-            <Card title={project.title}>
-                {/*  <h2 className="text-xl font-bold">{project.title}</h2> */}
-                {<p className="text-sm text-gray-500">Created At: {new Date(project.createdAt!).toLocaleDateString()}</p>}
-                <p className="text-sm text-gray-700">Created By: {project.createdBy as string}</p>
-                <p className="text-sm text-gray-700">Status: <span className={`project-badge status-${project.status}`}>{project.status}</span></p>
-                <Divider />
+            <div className="header">
+                <h2>{project.title}</h2>
+                <p>Created At: {new Date(project.createdAt!).toLocaleDateString()}</p>
+                <p>Created By: {project.createdBy as string}</p>
+                <p>Status: <span className={`project-badge status-${project.status}`}>{project.status}</span></p>
+            </div>
+            <Divider />
+            <div className="summary">
+                <h4>Summary</h4>
                 <p>{project.summary}</p>
-                <>
-                    <TabView>
-                        <TabPanel header="Collaborators">
-                            {/* Collaborators list / Accept-Decline UI */}
-                        </TabPanel>
-                        <TabPanel header="Themes">
-                            {/* Themes list / content */}
-                        </TabPanel>
-                        <TabPanel header="Phases">
-                            {/* Phases / timeline */}
-                        </TabPanel>
-                    </TabView>
-                </>
-            </Card>
+            </div>
+            <>
+                <TabView>
+                    <TabPanel header="Collaborators">
+                        {/* Collaborators list / Accept-Decline UI */}
+                    </TabPanel>
+                    <TabPanel header="Themes">
+                        {/* Themes list / content */}
+                    </TabPanel>
+                    <TabPanel header="Phases">
+                        {/* Phases / timeline */}
+                    </TabPanel>
+                </TabView>
+            </>
+
 
 
 
