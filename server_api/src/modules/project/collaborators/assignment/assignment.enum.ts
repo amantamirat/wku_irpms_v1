1db7bf3c0686d31baa29fd14713aa214f6ed581e
unknown <amweezy2@gmail.com>
2025-09-24 10:54:44 +0300

    project assignment started

---
+++ server_api/src/modules/project/collaborators/assignment/assignment.enum.ts
@@ -0,0 +1,16 @@
+export enum CollaboratorRole {
+  PI = "PI",
+  CO_PI = "CO_PI",
+  INVESTIGATOR = "INVESTIGATOR",
+  ASSISTANT = "ASSISTANT",
+  PROJECT_MANAGER = "PROJECT_MANAGER",
+  ADVISOR = "ADVISOR",
+  COMMUNITY_PARTNER = "COMMUNITY_PARTNER",
+}
+
+export enum AssignmentType {
+  PROJECT = "project",
+  THEME = "theme",
+  PHASE = "phase",
+  OBJECTIVE = "objective",
+}
---
+++ server_api/src/modules/project/collaborators/assignment/assignment.model.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+import { AssignmentType, CollaboratorRole } from "./assignment.enum";
+import { COLLECTIONS } from "../../../../enums/collections.enum";
+
+export interface ICollaboratorAssignment extends Document {
+    collaborator: mongoose.Types.ObjectId;
+    role: CollaboratorRole;
+    assignmentType: AssignmentType;
+    assignmentId?: mongoose.Types.ObjectId;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const CollaboratorAssignmentSchema = new Schema<ICollaboratorAssignment>(
+    {
+        collaborator: {
+            type: Schema.Types.ObjectId,
+            ref: COLLECTIONS.COLLABORATOR,
+            required: true,
+        },
+        role: {
+            type: String,
+            enum: Object.values(CollaboratorRole),
+            required: true,
+        },
+        assignmentType: {
+            type: String,
+            enum: Object.values(AssignmentType),
+            required: true,
+        },
+        assignmentId: {
+            type: Schema.Types.ObjectId,
+            required: function () {
+                return this.assignmentType !== AssignmentType.PROJECT;
+            },
+            refPath: "assignmentType",
+        },
+    },
+    { timestamps: true }
+);
